5329dbc6ee4327cfc807e9ac60bf8712c6b957c6
SpongyBacon <spongybacon@hotmail.co.uk>
2016-03-05 00:03:36 +0000

    Comments

--- src/main/java/io/sponges/bot/api/event/framework/EventManager.java
+++ src/main/java/io/sponges/bot/api/event/framework/EventManager.java
@@ -4,10 +4,29 @@ import java.util.function.Consumer;
 
 public interface EventManager {
 
+    /**
+     * Registers a new event listener
+     * @param event the event to listen to
+     * @param consumer the listener
+     * @param <T> class extending Event
+     * @return if registration was successful
+     */
     <T extends Event> boolean register(Class<T> event, Consumer<T> consumer);
 
+    /**
+     * Un-registers an event listener
+     * @param consumer event listener
+     * @param <T> class extending Event
+     * @return if un-registration was successful
+     */
     <T extends Event> boolean unregister(Consumer<T> consumer);
 
+    /**
+     * Sends a new event
+     * @param event the event to send
+     * @param <T> class extending Event
+     * @return TODO explain what #post returns
+     */
     <T extends Event> T post(T event);
 
 }
--- src/main/java/io/sponges/bot/api/module/Module.java
+++ src/main/java/io/sponges/bot/api/module/Module.java
@@ -11,37 +11,75 @@ public abstract class Module {
 
     private final String id, version;
 
+    /**
+     * Instantiates the module
+     * @param id the identifier of the module
+     * @param version the current module version
+     */
     public Module(String id, String version) {
         this.id = id;
         this.version = version;
     }
 
+    /**
+     * Abstract method invoked when the module is loaded & ready to enable
+     */
     public abstract void onEnable();
 
+    /**
+     * Abstract method invoked when the module is ready to disable
+     */
     public abstract void onDisable();
 
+    /**
+     * Internal method to instantiate module dependencies
+     * @param moduleLogger module logger instance
+     * @param eventManager server event manager
+     * @param commandManager server command manager
+     */
     public void init(ModuleLogger moduleLogger, EventManager eventManager, CommandManager commandManager) {
         this.moduleLogger = moduleLogger;
         this.eventManager = eventManager;
         this.commandManager = commandManager;
     }
 
+    /**
+     * Gets the module identifier
+     * @return module id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the module version
+     * @return module version
+     */
     public String getVersion() {
         return version;
     }
 
+    /**
+     * Gets the module's logger instance
+     * @return Modulelogger instance
+     * TODO swap for java.logging.Logger
+     */
     public ModuleLogger getLogger() {
         return moduleLogger;
     }
 
+    /**
+     * Gets the server's event manager instance
+     * @return EventManager instance
+     */
     public EventManager getEventManager() {
         return eventManager;
     }
 
+    /**
+     * Gets the server's command manager instance
+     * @return COmmandManager instance
+     */
     public CommandManager getCommandManager() {
         return commandManager;
     }
