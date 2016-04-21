a68108f06e7a8d5514fe5884c690d3510b23040b
SpongyBacon <spongybacon@hotmail.co.uk>
2016-04-22 00:06:39 +0100

    Op checks, permission registration

--- src/main/java/io/sponges/bot/api/cmd/Command.java
+++ src/main/java/io/sponges/bot/api/cmd/Command.java
@@ -1,6 +1,11 @@
 package io.sponges.bot.api.cmd;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Command {
+
+    private final List<String> permissions = new ArrayList<>();
 
     private final String description;
     private final String[] names;
@@ -12,11 +17,23 @@ public abstract class Command {
 
     public abstract void onCommand(CommandRequest request, String[] args);
 
+    protected void registerPermission(String permission) {
+        permissions.add(permission);
+    }
+
     public String getDescription() {
         return description;
     }
 
     public String[] getNames() {
         return names;
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+
+    public boolean requiresOp() {
+        return false;
     }
 }
--- src/main/java/io/sponges/bot/api/event/events/cmd/CommandProcessedEvent.java
+++ src/main/java/io/sponges/bot/api/event/events/cmd/CommandProcessedEvent.java
@@ -1,15 +1,22 @@
 package io.sponges.bot.api.event.events.cmd;
 
+import io.sponges.bot.api.cmd.Command;
 import io.sponges.bot.api.cmd.CommandRequest;
 
 public final class CommandProcessedEvent extends CommandEvent {
 
+    private final Command command;
     private final CommandRequest commandRequest;
     private final String[] args;
 
-    public CommandProcessedEvent(CommandRequest commandRequest, String[] args) {
+    public CommandProcessedEvent(Command command, CommandRequest commandRequest, String[] args) {
+        this.command = command;
         this.commandRequest = commandRequest;
         this.args = args;
+    }
+
+    public Command getCommand() {
+        return command;
     }
 
     public CommandRequest getCommandRequest() {
